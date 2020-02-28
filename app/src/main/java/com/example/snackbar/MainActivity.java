b344e8120297b862c29517bae36e6b6cb00c364f
Aldenir Rodrigues <aldenir_19@hotmail.com>
2020-02-28 15:33:17 -0300

    Finalizando

--- app/src/main/java/com/example/snackbar/MainActivity.java
+++ app/src/main/java/com/example/snackbar/MainActivity.java
@@ -10,17 +10,39 @@ import android.widget.SeekBar;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+  private  Button btnSnackbar, btnfechar;
+  private Snackbar snackbar;
 
   @Override
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Button btnSnackbar = findViewById(R.id.button);
+    btnSnackbar = findViewById(R.id.button);
+    btnfechar = findViewById(R.id.buttonFechar);
+
     btnSnackbar.setOnClickListener(new View.OnClickListener( ) {
       @Override
       public void onClick (View view) {
-        Snackbar.make(view, "Botão pressionado", Snackbar.LENGTH_LONG).show();
+        //Snackbar.make(view, "Botão pressionado", Snackbar.LENGTH_LONG).show();
+        //Snackbar.make(view, "Botão pressionado", Snackbar.LENGTH_INDEFINITE).show();
+        //Snackbar.make(view, "Botão pressionado", Snackbar.LENGTH_LONG).setAction("Confirmar", new View.OnClickListener( ) {
+        snackbar = Snackbar.make(view, "Botão pressionado", Snackbar.LENGTH_INDEFINITE).setAction("Confirmar", new View.OnClickListener( ) {
+
+          @Override
+          public void onClick (View v) {
+            btnSnackbar.setText("Botão abrir alterado!");
+          }
+        });
+        snackbar.show( );
+      }
+    });
+
+    btnfechar.setOnClickListener(new View.OnClickListener( ) {
+      @Override
+      public void onClick (View v) {
+        
+        snackbar.dismiss( );
       }
     });
   }
